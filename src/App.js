7dbe6879c7f420e70b8f6d2404dc02d1d73462b8
Ahmed ben Khlifa <ahmedjaidi4@gmail.com>
2023-11-23 21:30:16 +0100

    plan map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Brand from './components/Brand/Brand';
-import Navbar from './components/Navbar/Navbar' ;
 import Partners from './components/partners/Partners';
 import Footer from './components/footer/Footer';
 import Home from './components/home/Home';
